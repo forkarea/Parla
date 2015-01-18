dbacf99439807864f36e88fa4b36202752c233f9
Ryfcia <paulina.ryfka@gmail.com>
2015-01-19 00:37:23 +0100

    zmiana online users

--- src/Mondo/SocialNetworkBundle/View/UserTable.php
+++ src/Mondo/SocialNetworkBundle/View/UserTable.php
@@ -13,29 +13,41 @@ $key = Session::getSessionData('key');
 $result = DB::query("SELECT id, name, mykey, last_notified FROM users WHERE TIMESTAMPDIFF(SECOND, last_notified, now()) < %s ORDER BY name", [9]);
 ?>
 
-<table border="2">
-<thead>
-    <tr>
-        <td colspan="2">Users online</td> 
-    </tr>
-    <tr>
-        <td>Key</td>
-        <td>Name</td>
-    </tr>
-</thead>
-<?php  if($result) if($result->num_rows > 0)
-    while($row = $result->fetch_assoc()) {
-        $me = $row['mykey']==$key;
-?>
-    <tr>
-        <?php if(!$me) { ?>
-            <td><?= $row['mykey'] ?></td>
-            <td><?= $row['name'] ?></td>
-        <?php } ?>
-    </tr> 
-<?php
-    }
-?>
-</table>
+<div class="container userTable">
+	<div class="row">
+		<div class="tableTitle">
+			<span class="glyphicon glyphicon-globe" aria-hidden="true"></span> 
+			users online:
+		</div>
+	</div>
+	<div class="row users" >
+		<?php  if($result) if($result->num_rows > 0)
+			while($row = $result->fetch_assoc()) {
+			$me = $row['mykey']==$key;
+		?>
+		<div class="row singleUser" style="background: white; margin-bottom:5px">
+			<?php if(!$me) { ?>
+				<div class="col-md-3 userAv">
+					<?php if(file_exists('../'.\Parameters::UPLOADS_DIR.'/'.$key)) { ?>
+						<img width="50px" height="50px" id="image" src="../<?= \Parameters::UPLOADS_DIR.'/'.$key ?>"/>
+					<?php } ?>
+				</div>
+				<div class="col-md-8 onlineUserInfo">
+					<div class="row">
+						<span class="name"><?= $row['name'] ?></span>
+					</div>
+					<div class="row">
+						<span style="color:#52CC7A; font-size:10px">(<?= $row['mykey'] ?>)</span>
+					</div>
+				</div>
+			<?php } ?>
+		</div>
+		<?php
+			}
+		?>
+	</div>
+</div>
+
+
 
 
