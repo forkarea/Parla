11aef3bf8fe75841c466e7413ac9f007b0193703
oprogramador <uomodislesia@gmail.com>
2015-01-20 07:42:53 +0100

    changes..

---
+++ src/Mondo/SocialNetworkBundle/View/Search.php
@@ -0,0 +1,17 @@
+<?php include 'Base.php' ?>
+<?php
+use Mondo\UtilBundle\Core\DB;
+use Mondo\SocialNetworkBundle\Entity\User;
+
+?>
+
+<?php startblock('javascripts') ?>
+<?php endblock() ?>
+<?php startblock('styles') ?>
+    <link rel="stylesheet" href="../components/bootstrap/css/bootstrap.min.css">
+<?php endblock() ?>
+<?php startblock('content') ?>
+
+asdda
+
+<?php endblock() ?>
