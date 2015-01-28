02976fd690eef7527eb565926569fd2615f8f86d
oprogramador <uomodislesia@gmail.com>
2015-01-28 08:46:40 +0100

    search - init

---
+++ src/Mondo/SocialNetworkBundle/JS/search.js
@@ -0,0 +1,38 @@
+function search() {
+    var city = $('#city').val();
+    var country = $('#country').val();
+    var age_from = $('#age_from').val();
+    var age_to = $('#age_to').val();
+    var gender = $('#gender').val();
+    var orientation = $('#orientation').val();
+
+    console.log('minAge='+minAge);
+    console.log('maxAge='+maxAge);
+    if(age_from=='over') age_from = maxAge+1;
+    if(age_to=='under') age_to = minAge-1;
+
+    var query = 
+            'SELECT name, mykey FROM users WHERE ';
+    if(city!='') query += 'city=":city" AND ';
+    if(country!='') query += 'country=":country" AND ';
+    if(age_from!='under') query += 'TIMESTAMPDIFF(YEAR, birth, now())>=:age_from AND ';
+    if(age_to!='over') query += 'TIMESTAMPDIFF(YEAR, birth, now())<=:age_to AND ';
+    if(gender!='') query += 'gender=":gender" AND ';
+    if(orientation!='') query += 'orientation=":orientation" AND ';
+    query += '1 ORDER BY mykey';
+    console.log('query='+query);
+    var url = 'app.php?action=ajax_query&query='+encodeURIComponent(query
+            )+'&args='+encodeURIComponent(JSON.stringify({city: city, country: country, age_from: age_from, age_to: age_to, gender: gender, orientation: orientation}));
+    $.get(url, function(data) {
+        console.log('data='+JSON.stringify(data));
+        display(data);
+    });
+    console.log('url='+url);
+}
+
+function display(data) {
+    $('#results').html('');
+    for(var i in data) {
+        $('#results').append('<div><img width="200" src="uploads/'+data[i].mykey+'"/><br/>'+data[i].name+'<br/>'+data[i].mykey+'</div>');
+    }
+}
--- src/Mondo/SocialNetworkBundle/View/Search.php
+++ src/Mondo/SocialNetworkBundle/View/Search.php
@@ -10,16 +10,70 @@
 use Mondo\UtilBundle\Core\DB;
 use Mondo\SocialNetworkBundle\Entity\User;
 
+$minAge = 18;
+$maxAge = 70;
 ?>
 
-<?php startblock('javascripts') ?>
-<?php endblock() ?>
 <?php startblock('styles') ?>
     <link rel="stylesheet" href="../components/bootstrap/css/bootstrap.min.css">
 <?php endblock() ?>
+<?php startblock('javascripts') ?>
+<script>
+    var minAge = <?= $minAge ?>;
+    var maxAge = <?= $maxAge ?>;
+</script>
+    <script src="../components/jquery/jquery-1.11.1.min.js"></script>
+    <script src="../components/bootstrap/js/bootstrap.js"></script>
+    <script src="../src/Mondo/SocialNetworkBundle/JS/search.js"></script>
+<?php endblock() ?>
 <?php startblock('content') ?>
 
-City: <input name="city" />
-Country: <input name="country" />
+City: <input name="city" id="city" />
+Country: <input name="country" id="country" />
+Age:
+From:
+<select class="selects" name="age_from" id="age_from">
+    <option value="under">Under <?= $minAge ?></option>
+    <?php
+    for($i=$minAge; $i<=$maxAge; $i++) {
+    ?>
+        <option value="<?= $i ?>"><?= $i ?></option>
+    <?php
+    }
+    ?>
+    <option value="over">Over <?= $maxAge ?></option>
+</select>
+To:
+<select class="selects" name="age_to" id="age_to">
+    <option value="under">Under <?= $minAge ?></option>
+    <?php
+    for($i=$minAge; $i<=$maxAge; $i++) {
+    ?>
+        <option value="<?= $i ?>"><?= $i ?></option>
+    <?php
+    }
+    ?>
+    <option value="over">Over <?= $maxAge ?></option>
+</select>
+Gender:
+<select class="selects" name="gender" id="gender">
+    <option value=""></option>
+    <option value="m">male</option>
+    <option value="f">female</option>
+    <option value="n">other</option>
+</select>
+Orientation:
 
+<select class="selects" name="orientation" id="orientation">
+    <option value=""></option>
+    <option value="hetero">heterosexual</option>
+    <option value="homo">homosexual</option>
+    <option value="bi">bisexual</option>
+    <option value="a">asexual</option>
+</select>
+<input onclick="search()" class="btn btn-success submit-btn" type="button" value="OK"/>
+<input onclick="window.location.href='app.php'" class="btn btn-success submit-btn" type="button" value="BACK"/>
+<div id="results">
+
+</div>
 <?php endblock() ?>
